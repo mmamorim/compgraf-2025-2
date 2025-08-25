0aaaad955795fb8a01ba65c6cfa2274f768c5fb2
Marcelo <marc.m.amorim@gmail.com>
2025-08-25 20:13:05 -0300

    v1

--- rasterLab/main.js
+++ rasterLab/main.js
@@ -2,11 +2,13 @@ import imageLab from "./imageLab/imageLab.js"
 import createFilters from './imageLab/createFilters.js'
 
 import brilho from "./filtros/brilho.js";
+import colorizar from "./filtros/colorizar.js";
 
 console.log('imageLab', imageLab);
 imageLab.setLoadButtonID('btnLoad')
 
 createFilters(imageLab, [
     { label: 'Brilho', method: brilho },
+    { label: 'Colorizar', method: colorizar },
 ])
 
